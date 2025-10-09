36fde502a9a8ba85cba729305ea7a553d4f5d4b8
Chris Gardiner-Bill <cbill.uk@gmail.com>
2025-10-10 09:56:18 +1100

    chore: adds daisyui and landing page

---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello there</h1>
+      <p class="py-6">
+        This page is currently under construction.
+      </p>
+    </div>
+  </div>
+</div>
+</template>
